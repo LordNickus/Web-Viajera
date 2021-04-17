c50826d2c1371a4e7000f2bb1383d0695817f536
LordNickus <79268371+LordNickus@users.noreply.github.com>
2021-04-17 19:38:12 -0300

    mas Javascript

--- js/htmlcentral.js
+++ js/htmlcentral.js
@@ -1,5 +1,30 @@
+var productContainer = document.querySelector('.redesTop')
 
+var redesSociales=[
+{ logoImagen : 'imagenes/Yout.jpg',
+  alterno :'youtube',
+  marcaImagen: 'Youtube',
+},           
+{ logoImagen : 'imagenes/Face.png',
+  alterno :'Facebook',
+  marcaImagen: 'Facebook', 
+},
+{ logoImagen : 'imagenes/Inst.jpg',
+  alterno :'insta',
+  marcaImagen: 'Instagram',
+},
+{ logoImagen : 'imagenes/Wats.jpg',
+  alterno :'wassp',
+  marcaImagen: 'Wassap',
+},
+]
 
+for(var i=0; i<redesSociales.length; i++){
+    productContainer.innerHTML += '<img src="' + redesSociales[i].logoImagen + '"alt="' + redesSociales[i].alterno +
+    '"width="30" height="30"><a>' + redesSociales[i].marcaImagen + '</a>'
+}
+
+// cartel de las cookies abajo // 
 var aceptar = document.querySelector('.cookie a')
 var mostrarmensaje = false
 var cartelcookies = document.querySelector('.cookie')
